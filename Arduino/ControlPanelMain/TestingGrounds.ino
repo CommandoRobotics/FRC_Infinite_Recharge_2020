2f7c31a77e6da9e644c42d5409ddd8de807e1b3a
IsaacIngram22 <iingram@students.casciahall.com>
2020-01-20 11:29:36 -0600

    Added telemetry and changed sensor ports

--- Arduino/ControlPanelMain/TestingGrounds.ino
+++ Arduino/ControlPanelMain/TestingGrounds.ino
@@ -4,15 +4,25 @@
 
 void setup() {
     Serial.begin(9600);
-    setupColorSensor();
+    Serial.println("Setting up color sensors");
+    setupAllColorSensors();
+    Serial.println("Color sensors set up successfully");
 }
 
 void loop() {
-    Serial.println("Red :");
-    Serial.println(getRed());
-    Serial.println("Green :");
-    Serial.println(getGreen());
-    Serial.println("Blue :");
-    Serial.println(getBlue());
-    delay(1000);
+    Serial.println("Red 1 :");
+    Serial.println(getRed(1));
+    Serial.println("Green 1 :");
+    Serial.println(getGreen(1));
+    Serial.println("Blue 1 :");
+    Serial.println(getBlue(1));
+    Serial.println("-----");
+    Serial.println("Red 2 :");
+    Serial.println(getRed(2));
+    Serial.println("Green 2 :");
+    Serial.println(getGreen(2));
+    Serial.println("Blue 2 :");
+    Serial.println(getBlue(2));
+    Serial.println("=====");
+    delay(2000);
 }
